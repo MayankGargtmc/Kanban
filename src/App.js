4bd65a7b393a127bc850b3c288c1458b1123aee5
MayankGargtmc <mayank_g@bt.iitr.ac.in>
2023-10-08 16:28:57 +0530

    2nd commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import "./App.css";
+import Board from './Components/Board/Board';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='app_navbar'>
+        <h2>Kanban</h2>
+      </div>
+      <div className='app_outer'>
+        <div className='app_boards'>
+          <Board />
+          <Board />
+          <Board />
+          <Board />
+          <Board />    
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
